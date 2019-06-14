4a26e8fe67ab065aefcea177a7576b4085b9a67f
Phuong Phan <phuongphantai@gmail.com>
2019-06-14 16:52:28 +0700

    merge from original

--- GiftedFormManager.js
+++ GiftedFormManager.js
@@ -6,7 +6,7 @@ function doValidateOne(k = '', value = undefined, validators = {}) {
   var result = [];
 
   for (var i = 0; i < validate.length; i++) {
-    if (validate[i].validator === 'undefined') { continue; }
+    if (!validate[i].validator || validate[i].validator === 'undefined') { continue; }
 
     var args = validate[i].arguments;
     args = !Array.isArray(args) ? [ args ] : args;
@@ -28,13 +28,9 @@ function doValidateOne(k = '', value = undefined, validators = {}) {
       return val !== undefined ? val : '';
     });
 
-    var isNot = validate[i].isNot || false
-
     if (typeof validate[i].validator === 'function') {
       isValid = validate[i].validator.apply(null, clonedArgs);
-      if(isNot){
-        isValid = !isValid;
-      }
+
       // handle custom validators
       result.push({
         validator: 'Custom',
@@ -53,18 +49,16 @@ function doValidateOne(k = '', value = undefined, validators = {}) {
         if (typeof clonedArgs[0] === 'string') {
           clonedArgs[0] = clonedArgs[0].trim();
         }
-        isValid = validatorjs[validate[i].validator].apply(null, clonedArgs);
-      }else{
-        if(value && value.length > 0){
-          isValid = validatorjs[validate[i].validator].apply(null, clonedArgs);
-        } else{
-          isValid = true;
-        }
       }
 
-      if(isNot){
-        isValid = !isValid;
+      // Validator ONLY accepts string arguments.
+      if (clonedArgs[0] === null || clonedArgs[0] === undefined) {
+        clonedArgs[0] = '';
+      } else {
+        clonedArgs[0] = String(clonedArgs[0]);
       }
+
+      isValid = validatorjs[validate[i].validator].apply(null, clonedArgs);
 
       result.push({
         validator: validate[i].validator,
@@ -309,6 +303,27 @@ class Manager {
     if (typeof this.stores[obj.formName].values[obj.name] === 'undefined') {
       this.stores[obj.formName].values[obj.name] = obj.value;
     }
+  }
+
+  getValidationErrors(validated, notValidMessage = '{TITLE} Invalid', requiredMessage = '{TITLE} Required') {
+    var errors = [];
+    if (validated.isValid === false) {
+      for (var k in validated.results) {
+        if (validated.results.hasOwnProperty(k)) {
+          for (var j in validated.results[k]) {
+            if (validated.results[k].hasOwnProperty(j)) {
+              if (validated.results[k][j].isValid === false) {
+                let defaultMessage = !!validated.results[k][j].value ? notValidMessage : requiredMessage;
+                errors.push(validated.results[k][j].message || defaultMessage.replace('{TITLE}', validated.results[k][j].title));
+                // displaying only 1 error per widget
+                break;
+              }
+            }
+          }
+        }
+      }
+    }
+    return errors;
   }
 }
 
--- widgets/RowValueWidget.js
+++ widgets/RowValueWidget.js
@@ -9,28 +9,38 @@ import {
 } from 'react-native';
 
 var WidgetMixin = require('../mixins/WidgetMixin.js');
-var GiftedSpinner = require('react-native-gifted-spinner');
+var TimerMixin = require('react-timer-mixin');
+
 
 module.exports = createReactClass({
-  mixins: [WidgetMixin],
+  mixins: [TimerMixin, WidgetMixin],
   
   getDefaultProps() {
     return {
-      type: 'LoadingWidget',
+      type: 'RowValueWidget',
       onPress: () => {},
+      disclosure: true,
     };
   },
-
+  
   render() {
     return (
       <View style={this.getStyle('rowContainer')}>
         <TouchableHighlight
-          onPress={this.props.onPress}
+          onPress={() => {
+            this.requestAnimationFrame(() => {
+              this.props.onPress();
+            });
+          }}
           underlayColor={this.getStyle('underlayColor').pop()}
           {...this.props} // mainly for underlayColor
         >
           <View style={this.getStyle('row')}>
-            <GiftedSpinner />
+            {this._renderImage()}
+            <Text numberOfLines={1} style={this.getStyle('title')}>{this.props.title}</Text>
+            <View style={this.getStyle('alignRight')}>
+              <Text numberOfLines={1} style={this.getStyle('value')}>{this.state.value}</Text>
+            </View>
           </View>
         </TouchableHighlight>
       </View>
@@ -54,5 +64,22 @@ module.exports = createReactClass({
       alignItems: 'center',
     },
     underlayColor: '#c7c7cc',
+    disclosure: {
+      transform: [{rotate: '-90deg'}],
+      marginLeft: 10,
+      marginRight: 10,
+      width: 11,
+    },
+    title: {
+      flex: 1,
+      fontSize: 15,
+      color: '#000',
+      paddingLeft: 10,
+    },
+    value: {
+      fontSize: 15,
+      color: '#c7c7cc',
+      paddingRight: 10,
+    },
   },
 });
